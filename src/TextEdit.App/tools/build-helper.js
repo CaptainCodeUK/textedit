e7550e2422fa237049d9c68b186d6677cc25457d
David Lewis <captain.code@outlook.com>
2025-11-16 05:00:30 +0000

    ci: add build-helper tool to generate electron-builder.json (tools path)

---
+++ src/TextEdit.App/tools/build-helper.js
@@ -0,0 +1,36 @@
+const manifestFileName = process.argv[2];
+// @ts-ignore
+const manifestFile = require('../../electron.manifest.json');
+const dasherize = require('dasherize');
+const fs = require('fs');
+
+const builderConfiguration = { ...manifestFile.build };
+if(process.argv.length > 3) {
+    builderConfiguration.buildVersion = process.argv[3];
+}
+if(builderConfiguration.hasOwnProperty('buildVersion')) {
+    // @ts-ignore
+    const packageJson = { name: dasherize(manifestFile.name || 'electron-net'), author: manifestFile.author || '', version: builderConfiguration.buildVersion, description: manifestFile.description || '' };
+    fs.writeFile('./package.json', JSON.stringify(packageJson), (error) => {
+        if(error) {
+            console.log(error.message);
+        }
+    });
+}
+
+const builderConfigurationString = JSON.stringify(builderConfiguration);
+const outDir = './obj/Host/bin';
+fs.mkdir(outDir, { recursive: true }, (err) => {
+    if(err) throw err;
+    fs.writeFile(outDir + '/electron-builder.json', builderConfigurationString, (error) => {
+        if(error) {
+            console.log(error.message);
+        }
+    });
+    const manifestContent = JSON.stringify(manifestFile);
+    fs.writeFile(outDir + '/electron.manifest.json', manifestContent, (error) => {
+        if(error) {
+            console.log(error.message);
+        }
+    });
+});
